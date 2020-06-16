88a3d9000bf09913dfc580551438d866b5bfbbef
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2020-06-16 20:33:12 +0800

    Initial Commit

---
+++ exercises/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exercises
+//
+//  Created by 冀宸 on 2020/6/16.
+//  Copyright © 2020 冀宸. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
